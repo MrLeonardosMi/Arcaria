9f17e70c53c0c9be654c96b5fd77ff1eed808da8
MrLeonardos <mrleonardos.game8@mail.ru>
2024-09-15 22:24:44 +0300

    Init

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+
 const withNextIntl = createNextIntlPlugin();
- 
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
+const nextConfig = {
+    output: "export",
+    basePath: "/arcadia",
+};
+
 export default withNextIntl(nextConfig);
